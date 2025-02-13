3daccd7f330455661c3f8ac3641dfeb58f9c3b56
muslimuwitondanishema <muslimuwitondanishema@gmail.com>
2025-02-13 08:37:44 +0000

    Add get all products

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ const express = require("express")
 
 const app = express()
 
+let products = [
+	{ id: 1, name: "soap", price: 2000 }
+]
+
+app.get("/products", (req, res) => {
+	res.json(products)
+})
+
+
 app.listen(3000, () => console.log('server started on port 3000'))
