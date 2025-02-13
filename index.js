26eac26f175d861ec92ced30953d9b6b5373951c
muslimuwitondanishema <muslimuwitondanishema@gmail.com>
2025-02-13 08:52:38 +0000

    Add create a products

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
 
 const app = express()
 
+app.use(express.json())
+
 let products = [
 	{ id: 1, name: "soap", price: 2000 }
 ]
@@ -16,5 +18,13 @@ app.get("/products/:id", (req, res) => {
 	res.json(product)
 })
 
+
+app.post("/products", (req, res) => {
+	const product = req.body
+
+	products.push({ id: products.length, ...product })
+	res.json({ message: "data updated", })
+
+})
 
 app.listen(3000, () => console.log('server started on port 3000'))
