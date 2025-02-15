895e113464d855970df6715efb42afd4f2532341
muslimuwitondanishema <muslimuwitondanishema@gmail.com>
2025-02-15 06:18:03 +0000

    Put services in a separate module

--- index.js
+++ index.js
@@ -1,56 +1,24 @@
 const express = require("express")
+const {
+	getProducts,
+	getOneProduct,
+	createProduct,
+	updateProduct,
+	deleteproduct
+} = require("./services")
 
 const app = express()
 
 app.use(express.json())
 
-let products = [
-	{ id: 1, name: "soap", price: 2000 }
-]
-
-const getProducts = (req, res) => {
-	res.json(products)
-}
-
 app.get("/products", getProducts)
-
-const getOneProduct = (req, res) => {
-	const product = products.find(p => p.id === +req.params.id)
-	res.json(product)
-}
 
 app.get("/products/:id", getOneProduct)
 
-const createProduct = (req, res) => {
-	const product = req.body
 
-	products.push({ id: products.length, ...product })
-	res.json({ message: "data updated", })
-
-}
 app.post("/products", createProduct)
 
-const updateProduct = (req, res) => {
-	const dataupdate = req.body
-
-	products = products.map(p => {
-		if (p.id === +req.params.id) {
-			return { ...p, ...dataupdate }
-		}
-
-		return p
-	})
-
-	res.json({ message: "data updated" })
-
-}
-
 app.put("/products/:id", updateProduct)
-
-const deleteproduct = (req, res) => {
-	products = products.filter(p => p.id !== +req.params.id)
-	res.json({ message: "deleted succesfully" })
-}
 
 app.delete("/products/:id", deleteproduct)
 
